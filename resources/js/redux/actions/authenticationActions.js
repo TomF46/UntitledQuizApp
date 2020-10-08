de6e459da3c3406225c51f1c40e298b2e27fb608
TomF46 <t-d-f@live.co.uk>
2020-10-08 20:05:23 +0100

    Add tailwind css framework

--- resources/js/redux/actions/authenticationActions.js
+++ resources/js/redux/actions/authenticationActions.js
@@ -4,32 +4,32 @@ import { beginApiCall, apiCallError } from "./apiStatusActions";
 import { saveUser, removeUser } from "../../tools/localStorage";
 
 export function userLoginSuccess(user) {
-  return { type: types.USER_LOGIN_SUCCESS, user };
+    return { type: types.USER_LOGIN_SUCCESS, user };
 }
 
 export function userLogoutSuccess(user) {
-  return { type: types.USER_LOGOUT_SUCCESS, user };
+    return { type: types.USER_LOGOUT_SUCCESS, user };
 }
 
 export function login(userLoginDetails) {
-  return function (dispatch) {
-    dispatch(beginApiCall());
-//     return authenticationApi
-//       .Login(userLoginDetails)
-//       .then((user) => {
-//         saveUser(user);
-//         dispatch(userLoginSuccess(user));
-//       })
-//       .catch((err) => {
-//         dispatch(apiCallError(err));
-//         throw err;
-//       });
-//   };
+    return function(dispatch) {
+        dispatch(beginApiCall());
+        //     return authenticationApi
+        //       .Login(userLoginDetails)
+        //       .then((user) => {
+        //         saveUser(user);
+        //         dispatch(userLoginSuccess(user));
+        //       })
+        //       .catch((err) => {
+        //         dispatch(apiCallError(err));
+        //         throw err;
+        //       });
+    };
 }
 
 export function logout(user) {
-  return function (dispatch) {
-    removeUser();
-    dispatch(userLogoutSuccess(user));
-  };
+    return function(dispatch) {
+        removeUser();
+        dispatch(userLogoutSuccess(user));
+    };
 }
