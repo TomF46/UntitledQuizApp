ec9822f2dd0174d4d21c92977c42ef1023e6ffd7
TomF46 <t-d-f@live.co.uk>
2020-10-13 16:23:15 +0100

    Rough unstyled page which gets quiz from api

--- resources/js/components/Quiz/Detail.js
+++ resources/js/components/Quiz/Detail.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getQuiz } from "../../api/quizApi";
 
-const QuizDetailPage = ({ history }) => {
-    return (
-        <div className="quiz-detail-page">
-            <p>Quiz Detail Page</p>
+const QuizDetailPage = ({quizId ,history }) => {
+    const [quiz, setQuiz] = useState(null);
+
+    useEffect(() => {
+        if(!quiz) {
+            getQuiz(quizId).then(quizData => {
+                setQuiz(quizData);
+            }).catch(error => {
+                console.log("Error getting quiz " + error);
+            });
+        }
+    }, [quizId, quiz])
+
+    return !quiz ? (
+        <p>... Loading quiz</p>
+    ) : (
+        <div>
+            <p>Detail for quiz</p>
+            <p>{quiz.title}</p>
+            <p>{quiz.description}</p>
         </div>
-    );
+    )
 };
 
+const mapStateToProps = (state, ownProps) => {
+    const quizId = ownProps.match.params.quizId;
+    return {
+      quizId
+    };
+  };
+
 QuizDetailPage.propTypes = {
+    quizId: PropTypes.any.isRequired,
     history: PropTypes.object.isRequired
 };
 
-export default QuizDetailPage;
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizDetailPage);
+
