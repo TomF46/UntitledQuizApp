01c4c2910eb2ab8628ed6f15b6711b5eb14089f5
TomF46 <t-d-f@live.co.uk>
2021-11-11 12:50:05 +0000

    Update from homestead to sail

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix');
 const tailwindcss = require("tailwindcss");
 
 /*
@@ -12,7 +12,7 @@ const tailwindcss = require("tailwindcss");
  |
  */
 
-mix.react("resources/js/app.js", "public/js").sourceMaps();
+mix.js("resources/js/app.js", "public/js").react().sourceMaps();
 mix.sass("resources/sass/app.scss", "public/css").options({
     processCssUrls: false,
     postCss: [tailwindcss("./tailwind.config.js")]
