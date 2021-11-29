3630244990629c216ba9df4a57a54e9f3f2d87e7
TomF46 <t-d-f@live.co.uk>
2021-11-29 14:29:13 +0000

    Challenge panel more eye catching

--- resources/js/components/Quiz/Play/ChallegeInfoPanel.js
+++ resources/js/components/Quiz/Play/ChallegeInfoPanel.js
@@ -4,15 +4,15 @@ import PropTypes from "prop-types";
 const ChallengeInfoPanel = ({ challenge }) => {
     return (
         <div className="overflow-hidden shadow">
-            <div className="grid grid-cols-12 px-2 py-1 overflow-hidden">
+            <div className="grid grid-cols-12 px-2 py-1 bg-secondary overflow-hidden">
                 <div className="hidden lg:block lg:col-span-3"></div>
                 <div className="col-span-6 lg:col-span-3 text-center">
-                    <p className="text-sm text-gray-600">Challenger</p>
-                    <p className="font-medium items-center">{challenge.challengerUsername}</p>
+                    <p className="text-sm text-white">Challenger</p>
+                    <p className="font-medium text-white items-center">{challenge.challengerUsername}</p>
                 </div>
                 <div className="col-span-6 lg:col-span-3 text-center">
-                    <p className="text-sm text-gray-600">Score required</p>
-                    <p className="font-medium items-center">{challenge.scorePercentToBeat}%</p>
+                    <p className="text-sm text-white">Score required</p>
+                    <p className="font-medium text-white items-center">{challenge.scorePercentToBeat}%</p>
                 </div>
                 <div className="hidden lg:block lg:col-span-3"></div>
             </div>
